6a64201beb1c3adb532d5529cb0b667a639bf96f
gaabakshita <93419960+gaabakshita@users.noreply.github.com>
2021-12-08 00:59:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+function fadefun()
+{
+    $(".home-text").animate({opacity:'1.0'},3000);
+}
+
+$(".joinbtn").click(
+    function()
+{
+    alert("Welcome to Brisfit Family!!");
+}
+)
+
+$(".carousel-item").click(
+    function()
+    {
+        alert("Join Us to get access to diet plan");
+    }
+)
+
